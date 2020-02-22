3319f342ea56df7557672b0a79e6881ea465db5a
Jeremy <jer@jer.cx>
2020-02-22 11:50:44 -0800

    impl getenv, putenv, setenv

---
+++ 6/environ/env.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *argv0;
+
+int envlen = -1;
+int envcap = -1;
+extern char **environ;
+char **jenviron;
+
+char **initenv()
+{
+	int len;
+	char **e, **j;
+
+	if (envcap != -1 && envlen != -1 && jenviron != NULL)
+		return jenviron;
+	for (e = environ; *e; e++);
+	envcap = envlen = (e - environ) - 1;
+	if ((jenviron = malloc(sizeof(char *) * (envcap+1))) == NULL) 
+		return NULL;
+	for (e = environ, j = jenviron; *e; e++, j++)
+	{
+		len = strlen(*e);
+		if ((*j = malloc(len * sizeof(char))) == NULL)
+			return NULL;
+		strcpy(*j, *e);
+	}
+	return jenviron;
+}
+
+
+int growenv()
+{
+	if (initenv() == NULL)
+		return -1;
+	if ((jenviron = realloc(jenviron, 2 * envcap * sizeof(char *))) == NULL)
+		return -1;
+	return 0;
+}
+
+int jputenv(char *s)
+{
+	if (initenv() == NULL)
+		return -1;
+	if ((envcap - envlen) < 1)
+		if (growenv() != 0)
+			return -1;
+
+	jenviron[envlen++] = s;
+	jenviron[envlen] = NULL;
+	return 0;
+}
+
+char *jgetenv(char *s)
+{
+	char **e;
+	int nlen = strlen(s);
+	if (initenv() == NULL)
+		return NULL;
+	for (e = jenviron; *e; e++)
+		if (strncmp(s, *e, nlen) == 0 && *e && (*e)[nlen] == '=')
+			return *e+nlen+1;
+}
+
+int jsetenv(const char *name, const char *value, int override)
+{
+	char **e, *d;
+
+	/* (environment)? (name|value) length */
+	int nlen, vlen, evlen;
+	nlen = strlen(name);
+	vlen = strlen(value);
+
+	if (initenv() == NULL)
+		return -1;
+
+	/* locate variable */
+	for (e = jenviron; *e; e++)
+		if (strncmp(name, *e, nlen) == 0 && *e && (*e)[nlen] == '=')
+			break;
+
+	/* if (env var exists & if there's not enough space) realloc */
+	if (*e) {
+		if (override == 0)
+			return -1;
+
+		evlen = strlen(*e) - (nlen+1);
+
+		/* if the new value len is bigger, realloc it */
+		if (vlen > evlen)
+			if ((*e = (char *) realloc(*e, (nlen+1+vlen) * sizeof(char))) == NULL)
+				return -1;
+		d = *e;
+	} else {
+		if ((d = (char *) malloc((nlen+1+vlen) * sizeof(char))) == NULL)
+			return -1;
+		sprintf(d, "%s=", name);
+		jputenv(d);
+	}
+
+	strcpy(d + nlen+1, value);
+	return 0;
+}
+
+void jprintenv()
+{
+	initenv();
+	char **e;
+	for (e = jenviron; *e; e++)
+		puts(*e);
+}
---
+++ 6/environ/env.h
@@ -0,0 +1,4 @@
+int jputenv(char *s);
+char *jgetenv(char *s);
+int jsetenv(const char *name, const char *value, int override);
+void jprintenv();
